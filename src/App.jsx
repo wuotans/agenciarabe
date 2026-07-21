cf84a2aff3d476ed771f89833de6b3adcb082f5f
Wuotans <107582914+wuotans@users.noreply.github.com>
2026-07-21 14:24:15 -0400

    Aplica ajustes finais de logos e cards compactos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '@/pages/Home';
 import ScrollToTop from '@/components/ScrollToTop';
 import './rabe-adjustments.css';
+import './compact-overrides.css';
 
 export default function App() {
   return (
