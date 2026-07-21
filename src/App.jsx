d02205afc72d1f8fc022475d468b270044ed948b
Wuotans <107582914+wuotans@users.noreply.github.com>
2026-07-21 14:18:47 -0400

    Carrega ajustes visuais das esferas e cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '@/pages/Home';
 import ScrollToTop from '@/components/ScrollToTop';
+import './rabe-adjustments.css';
 
 export default function App() {
   return (
